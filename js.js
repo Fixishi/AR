e66cdbb4b92cafb8df5a0c33b622bccb038674a5
Fixishi <filip.olic01@gmail.com>
2024-04-06 08:08:30 +0200

    Docs

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-function changeModel(src) {
-    const mainModel = document.getElementById('main-model');
-    mainModel.src = src;
+function changeModel(src) {     // Function to change the model in the main model viewer
+    const mainModel = document.getElementById('main-model');    // Get the main model viewer
+    mainModel.src = src;   // Change the model to the new model
 }
